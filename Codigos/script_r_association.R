a35c87b6dd6a7bf80fcda283f79b2f5e3d2f4338
Junior Clementino <juniorclementino45@gmail.com>
2019-09-18 12:16:10 -0300

    R_files and csv

--- Codigos/script_r_association.R
+++ Codigos/script_r_association.R
@@ -1,37 +1,34 @@
-## Caregando os pacotes necessários
+#install.packages("arules")
 library(arules)
-library(arulesViz)
 
-#Carregando o conjunto de dados
-#data("Name_file")
 
-#Inspecionando 
-name_file <- read.transactions(file -'name_file.txt', sep - ',', rm.duplicates -r)
+base = read.transactions(file.choose(),header = F, sep=",", rm.duplicates = T)
+summary(base)
 
-#Sumário 
-summary(name_file)
 
 #Observando algumas linhas
-inspect(name_file[1:4])  #linhas da matriz são cestas de compras
+inspect(base[1:4])  #linhas da matriz são cestas de compras
 
 #colunas da matriz são itens
-itemFrequency(name_file[,1:10]) #vendo  os numero
-itemFrequencyPlot(name_file[,1:10]) #Grafico de barras
+itemFrequency(base[,1:10]) #vendo  os numero
+itemFrequencyPlot(base[,1:10]) #Grafico de barras
+
 
 #É possivel colocar no gráfico somente itens com um certo suporte
-itemFrequencyPlot(name_file, support =0.1) # ou - 0.1
+itemFrequencyPlot(base, support =0.2) # ou - 0.1
+
 
 ## É possivel fazer seus próprios gráficos
-itens <- itemFrequency(name_file)
+itens <- itemFrequency(base)
 itens <- itens[order(itens, decreasing = T)]
 dotchart(itens)
 
 # O mesmo gráfico com menos itens
 dotchart(itens[1:10], cex = 0.8)
-itemFrequencyPlot(name_file, topN=10)
+itemFrequencyPlot(base, topN=10)
 
 ##Encontrando as regras de associacao com o algortimo Aprori
-regras <- apriori(name_file, parameter  =list(support=0.001, confidence=0.5, minlen=2))
+regras <- apriori(base, parameter  =list(support=0.001, confidence=0.5, minlen=2))
 
 #inspecionando as regras 
 summary(regras)
@@ -39,7 +36,7 @@ summary(regras)
 #Vendo as regras 
 inspect(regras[1:3])
 inspect(regras)
-inspectsort((regras by = 'confidence'))
+inspectsort((regras, by = 'confidence'))
 
 #Visualizando as regras como um grafo
 plot(regras, method = 'scatterplot', interactive=True, shading=NA)
@@ -60,6 +57,6 @@ plot(regras, method="grouped", control = list(k=50),cex=0.8)
 
 
 #Plotando regras como grafos
-subregras2 <- head(sort(regras, by "lift"),10)
+subregras2 <- head(sort(regras, by ="lift"),10)
 pdf(file - "grafos.pdf")
 plot(subregras2, method="graph", control=list(tyoe="itemsets"))
